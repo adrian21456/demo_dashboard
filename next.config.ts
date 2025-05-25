0f16bd6602fe77f4eee1958781e06f4b048c02cc
Adrian Duma-op <46225266+adrian21456@users.noreply.github.com>
2025-05-25 14:38:09 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/shadcn',
-  assetPrefix: '/shadcn',
+  // basePath: '/shadcns',
+  // assetPrefix: '/shadcn',
   eslint: {
     ignoreDuringBuilds: true,
   },
